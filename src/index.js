915d6e9e6eb48176e9fe8b164cae42de4f2d0177
TonicB <a.sharafiev@gmail/com>
2023-01-17 17:49:48 +0200

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = {}
+  bracketsConfig.forEach(x => config[x[0]] = x[1])  
+  let stack = []
+  let res
+  let resbtw = true
+  let strArr = Array.from(str)
+  strArr.forEach(x => {!Object.values(config).includes(x) ? stack.push(x) : !Object.keys(config).includes(x) ?  config[stack[stack.length - 1]] == x ? stack.pop() : resbtw = false : stack[stack.length -1] == x ? stack.pop() : stack.push(x)})
+  resbtw? stack.length == 0 ? res = true : res = false : res = false
+  return res
 }
